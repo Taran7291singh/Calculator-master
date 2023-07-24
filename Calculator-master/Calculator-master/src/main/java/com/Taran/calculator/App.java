4a82381e67a5ceb6437ae7757e4470ee1a6acc98
Taranpreet singh <103059880+Taran7291singh@users.noreply.github.com>
2023-07-24 23:30:42 +0530

    Add files via upload

---
+++ Calculator-master/Calculator-master/src/main/java/com/Taran/calculator/App.java
@@ -0,0 +1,10 @@
+package com.Taran.calculator;
+
+import com.Taran.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
